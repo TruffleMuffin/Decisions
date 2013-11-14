8f00d05a97375b174fa339958dc4755a9a069934
Gareth Thomas <gareth.thomas@cdsm.co.uk>
2013-11-14 17:26:44 +0000

    decribing decide endpoint

--- Securables/Securables/API/DecideController.cs
+++ Securables/Securables/API/DecideController.cs
@@ -1,4 +1,5 @@
-﻿using Securables.Contracts;
+﻿using System;
+using Securables.Contracts;
 using System.Threading.Tasks;
 using System.Web.Http;
 
@@ -11,11 +12,16 @@ namespace Securables.API
     {
         /// <summary>
         /// Handles a Decision request.
-        /// 
         /// HTTP Verb: GET
         /// </summary>
-        /// <returns>A <see cref="Decision"/> indicating the result of the query</returns>
-        public async Task<Decision> Get()
+        /// <param name="componentName">Name of the component the decision should be run for.</param>
+        /// <param name="userId">The user id.</param>
+        /// <param name="roleName">Name of the role the user claims to have on the entity within the <see cref="componentName"/>.</param>
+        /// <param name="entityId">The entity id.</param>
+        /// <returns>
+        /// A <see cref="Decision" /> indicating the result of the query.
+        /// </returns>
+        public async Task<Decision> Get(string componentName, Guid userId, string roleName, string entityId)
         {
             return await Task.FromResult(Decision.Deny);
         }
--- Securables/Securables/Global.asax.cs
+++ Securables/Securables/Global.asax.cs
@@ -25,7 +25,7 @@ namespace Securables
             // Decision routes
             routes.MapHttpRoute(
                 name: "DecideApi",
-                routeTemplate: "Api/Decide/",
+                routeTemplate: "Api/Decide/{componentName}/{userId}/{roleName}/{entityId}",
                 defaults: new { controller = "Decide", action = "Get" },
                 constraints: new { httpMethod = new HttpMethodConstraint(HttpMethod.Get) });
         }
