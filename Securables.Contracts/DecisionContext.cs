d2b605e47d6cf6b54f863323047966cbf197bb54
Gareth Thomas <gareth.thomas@cdsm.co.uk>
2013-11-14 17:51:31 +0000

    including initial classes describing decisions

---
+++ Securables.Contracts/DecisionContext.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Securables.Contracts
+{
+    /// <summary>
+    /// Describes a decision context, containing all relevant information for getting a <see cref="Decision"/> result from the <see cref="ISecurablesService"/>.
+    /// </summary>
+    public class DecisionContext
+    {
+        /// <summary>
+        /// The string format for the Id of this instance
+        /// </summary>
+        private const string ID_FORMAT = "{0}/{1}/{2}/{3}";
+
+        /// <summary>
+        /// Gets or sets the Globally Unique Identifier that identifies this specific instance uniquely.
+        /// </summary>
+        public string Id { get { return string.Format(ID_FORMAT, Component, UserId, Role, EntityId); } }
+
+        /// <summary>
+        /// Gets or sets the name of the component which the decision should be made within.
+        /// </summary>
+        public string Component { get; set; }
+
+        /// <summary>
+        /// Gets or sets the user id the decision should be made for.
+        /// </summary>
+        public Guid UserId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the role the decision should be about.
+        /// </summary>
+        public string Role { get; set; }
+
+        /// <summary>
+        /// Gets or sets the entity id which the decision should be on.
+        /// </summary>
+        public string EntityId { get; set; }
+    }
+}
---
+++ Securables.Contracts/ISecurablesService.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace Securables.Contracts
+{
+    /// <summary>
+    /// Describes a operations that can be performed using Securables.
+    /// </summary>
+    public interface ISecurablesService
+    {
+        /// <summary>
+        /// Determines the result of the specified <see cref="context"/>.
+        /// </summary>
+        /// <param name="context">The context.</param>
+        /// <returns>
+        /// A <see cref="Decision" /> indicating the result of the query.
+        /// </returns>
+        Decision CheckAsync(DecisionContext context);
+
+        /// <summary>
+        /// Determines the results of the specified <see cref="contexts"/>.
+        /// </summary>
+        /// <param name="contexts">The contexts.</param>
+        /// <returns>
+        /// A set of <see cref="Decision" /> indicating the results of the query.
+        /// </returns>
+        IDictionary<string, Decision> CheckAsync(IEnumerable<DecisionContext> contexts);
+    }
+}
