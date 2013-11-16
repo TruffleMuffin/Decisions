7ff3c75128fe60e632a3705ca9d97686a7c89730
Gareth Thomas <gareth.thomas@cdsm.co.uk>
2013-11-16 13:39:28 +0000

    fixes #3
    Implemented a simple policyprovider that should deal with most
    non-bespoke requirements

--- Securables.Tests/Application/Services/SecurablesServiceTests.cs
+++ Securables.Tests/Application/Services/SecurablesServiceTests.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Threading.Tasks;
 using MbUnit.Framework;
 using Securables.Application.Services;
 using Securables.Contracts;
+using Securables.Contracts.Providers;
 using Securables.Tests.Support;
 
 namespace Securables.Tests.Application.Services
@@ -19,7 +21,18 @@ namespace Securables.Tests.Application.Services
         void SetUp()
         {
             environmentService = new EnvironmentService(new[] { new ExampleEnvironmentProvider() });
-            policyService = new PolicyService(new[] { new ExamplePolicyProvider() }, environmentService);
+            var policies = new Dictionary<string, AbstractPolicy>
+                {
+                    { "A", new AlphaPolicy() }, 
+                    { "B", new BetaPolicy() }, 
+                    { "C", new AlphaPolicy() }, 
+                    { "D", new AlphaPolicy() }, 
+                    { "E", new CappaPolicy {CurrentUserEnvironmentKey = "CurrentUser", MatchUserId = new Guid("880A00AD-5C40-447B-821A-2679E757B267")} }, 
+                    { "F", new CappaPolicy {CurrentUserEnvironmentKey = "CurrentUser", MatchUserId = new Guid("1E9A7C0C-FC86-4516-BA42-F7232E65A12C")} }, 
+                    { "G", new DeltaPolicy {AclEnvironmentKey = "Acl"} }, 
+                    { "H", new DeltaPolicy {AclEnvironmentKey = "LongRunning"} }
+                };
+            policyService = new PolicyService(new[] { new PolicyProvider(policies) }, environmentService);
             target = new SecurablesService(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Securables.config"), policyService);
         }
 
